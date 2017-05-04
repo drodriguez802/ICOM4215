9967c596efd9851509af500f18937b8e4598c1ad
drodriguez802 <daniel.rodriguez22@upr.edu>
2017-05-04 18:56:15 -0400

    Working Micro Store
    Works for the given states
    TO-DO:
    +Address Selector, Conditions and Inverter
    +Decode and find state from given Instruction

--- microStore.v
+++ microStore.v
@@ -1,6 +1,6 @@
 //ROM For Load immediate Offset and Register Offset
 module encoder(instruction, state);
-   output reg [7:0]state;
+   output reg [7:0]state = 8'b00000000;
    input wire [31:0] instruction;
 
    always@(instruction)
@@ -20,19 +20,22 @@ module encoder(instruction, state);
             $display("STATE 11");
             state = 8'b00001011;
             end
+        $display("STATE: %b", state);
     end
 endmodule
 //ROM For Load immediate Offset and Register Offset
-module ROM(MA,MB,MC,MBS,MBSMRF,MUXMDR,MDREn,MAREn,IREn,SHF_S,ShiftEn,RFEn,RW,MemEn,MOC, Inv, N2, N1, N0,SignExtSel,CR,OP, state);
+module ROM(MA,MB,MC,MBS,MBSMRF,MUXMDR,MDREn,MAREn,IREn,SHF_S,ShiftEn,RFEn,RW,MemEn,MOC, Inv, N2, N1, N0,SignExtSel,CR,OP, state, clk);
    output reg MBS,MUXMDR,MDREn,MAREn,IREn,SHF_S,ShiftEn,RFEn,RW,MemEn,MOC, MOV,DataType, Inv, N2, N1, N0, S1, S0;
    output reg [1:0]SignExtSel,MA,MB,MBSMRF;
    output reg [2:0] MC;
    output reg [5:0] CR;
    output reg [4:0]OP;
    input wire [7:0] state;
+   input wire clk;
 
-   always@(state)
+   always@(posedge clk)
     begin
+    $display("INSIDE ROM STATE IS: %b",state);
         if(state==8'b00000000)
             begin
             $display("STATE 0");
@@ -273,62 +276,11 @@ module ROM(MA,MB,MC,MBS,MBSMRF,MUXMDR,MDREn,MAREn,IREn,SHF_S,ShiftEn,RFEn,RW,Mem
     end
 endmodule
 
-//Next State Address Selector
-module nextStAddSel(M, Sts, N2, N1, N0);
-   input wire Sts;
-   input wire S1, S0;
-   input wire N2, N1, N0;
-   output reg [1:0] M;
-   wire [2:0] nSelect;
-   assign nSelect[0] = N0;
-   assign nSelect[1] = N1;
-   assign nSelect[2] = N2;
-   always@(nSelect or N2 or N1 or N0)
-    begin
-        if(nSelect==000)
-            M<=00;
-        else if(nSelect==3'b001)
-            M<=01;
-        else if(nSelect==3'b010)
-            M<=10;
-        else if(nSelect==3'b011)
-            M=11;
-        else if(nSelect==3'b100 && ~Sts)
-            M=00;
-        else if(nSelect==3'b100 && Sts)
-            M=10;
-        else if(nSelect==3'b101 && ~Sts)
-            M=11;
-        else if(nSelect==3'b101 && Sts)
-            M=10;
-        else if(nSelect==3'b110 && ~Sts)
-            M=11;
-        else if(nSelect==3'b110 && Sts)
-            M=00;
-        else if(nSelect==3'b111)
-            M=00;
-    end
-endmodule
-
-module inverter(iBit, inv, oBit);
-    input wire iBit, inv;
-    output reg oBit;
-    always@(iBit or inv)
-    begin
-        if(inv==1'b1)
-            oBit=~iBit;
-        else if(inv==1'b0)
-            oBit=iBit;
-    end
-endmodule
-
-module multiplexer4x1(outR, inR0, inR1, inR2, inR3, S1, S0);
+module multiplexer4x1(outR, inR0, inR1, inR2, inR3, select);
    input wire S1, S0;
    input [7:0] inR0, inR1, inR2, inR3;
    output reg [7:0] outR;
-   wire [1:0] select;
-   assign select[0] = S0;
-   assign select[1] = S1;
+   input [1:0] select;
    always@(select or inR0 or inR1 or inR2 or inR3)
     begin
         case(select)
@@ -337,58 +289,69 @@ module multiplexer4x1(outR, inR0, inR1, inR2, inR3, S1, S0);
             2'b10 : outR = inR2;
             2'b11 : outR = inR3;
     endcase
+    // $display("SELECT WAS: %b",select);
+    // $display("MUX OUT: %b",outR);
     end
 endmodule
 
-module adder(value, add);
-   input [7:0] value;
-   output  wire [7:0] add;
-   wire one = 1'b1;
-   
-   assign add = value+one;
-   
+module adder(input [7:0] value,input [7:0] one,output  [7:0] result);
+        wire [7:0]result2 = value + one;
+		assign result = result2;
+		always@(result)
+		begin
+		    $display("RESULT: ",result);
+		    end
 endmodule
 
-module incrementalRegister(out, in);
-    input wire [7:0] in;
-    output wire [7:0] out;
-        assign out = in;
-endmodule
-
-
-
-    
 
 module main;
-  wire [7:0] state = 8'b00000000;
+  wire [7:0] state;
   wire [7:0] stateEncoder;
   reg [31:0] instruction;
   wire [31:0] out;
   wire [7:0] outMux;
   wire MBS,MUXMDR,MDREn,MAREn,IREn,SHF_S,ShiftEn,RFEn,RW,MemEn,MOC, Inv, N2, N1, N0, S1, S0, Sts,iBit, inv;
   wire [1:0]SignExtSel,MA,MB,MBSMRF;
-  wire [2:0] MC, M;
+  wire [2:0] MC;
   wire [5:0] CR;
   wire [4:0] OP;
-  wire [7:0] add;
-   
-  ROM rom(MA,MB,MC,MBS,MBSMRF,MUXMDR,MDREn,MAREn,IREn,SHF_S,ShiftEn,RFEn,RW,MemEn,MOC, Inv, N2, N1, N0,SignExtSel,CR,OP, state);
+  wire [7:0] outAdd;
+  reg [1:0] M;
+  reg clk;
   encoder enc(instruction, stateEncoder);
-  multiplexer4x1 mux(outMux, stateEncoder, 0, CR, add, M[1], M[0]);
-  adder addTo(outMux, add);
-  nextStAddSel addSelect(M, Sts, N2, N1, N0);
-  inverter invert(iBit, inv, Sts);
+  multiplexer4x1 mux(outMux, stateEncoder, 0, CR, outAdd, M);
+  adder add(outMux, 8'b00000001,outAdd);
+  ROM rom(MA,MB,MC,MBS,MBSMRF,MUXMDR,MDREn,MAREn,IREn,SHF_S,ShiftEn,RFEn,RW,MemEn,MOC, Inv, N2, N1, N0,SignExtSel,CR,OP, outMux,clk);
   initial
   begin
-      $display("INSTRUCTION = %b",instruction);
-      $display("STATE ENCODER = %b",stateEncoder);
-      $display("OUTPUT MUX = %b",outMux);
+    //   $display("INSTRUCTION = %b",instruction);
+    //   $display("STATE  = %b",state);
+    //   $display("OUTPUT MUX = %b",outAdd);
       $display("MA  |  MB  |  MC  |  MBS  |  MBSMRF  |  MUXMDR  |  MDREn  |  MAREn  |  IREn  |  SHF_S  |  ShiftEn  |  RFEn  |  RW  |  MemEn  |  MOC  |   Inv  |   N2  |   N1  |   N0  |  SignExtSel  |  CR      |  OP ");
       $monitor("%b     %b     %b     %b      %b            %b        %b        %b          %b        %b          %b           %b      %b        %b       %b       %b         %b       %b      %b              %b     %b      %b",MA,MB,MC,MBS,MBSMRF,MUXMDR,MDREn,MAREn,IREn,SHF_S,ShiftEn,RFEn,RW,MemEn,MOC, Inv, N2, N1, N0,SignExtSel,CR,OP);
       
-      #5 instruction = 32'b11110010000000000000000000000000;
-      #5 instruction = 32'b11111010000000000000000000000000;
+       #5 clk = 1'b1;
+       #10 M = 2'b00;
+       #15 clk = 1'b0;
+       #20 clk = 1'b1;
+       #25 M = 2'b11;
+       #30 clk = 1'b0;
+       #35 clk = 1'b1;
+       #40 clk = 1'b0;
+       #45 clk = 1'b1;
+       #50 clk = 1'b0;
+       #55 clk = 1'b1;
+       #60 clk = 1'b0;
+       #55 clk = 1'b1;
+       #60 clk = 1'b0;
+       #65 clk = 1'b1;
+       #70 clk = 1'b0;
+       #75 clk = 1'b1;
+       #80 clk = 1'b0;
+    //   #5 state = 8'b00000001;
+    //   #5 state = 8'b00000010;
+    //   #5 state = 8'b00000011;
+    //   #5 state = 8'b00000100;
       
       end
  endmodule
-
